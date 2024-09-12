f66e38cb2779d431400f9f1fc875a1bc37a03ba8
LalitRade <lalitr.jaro@gmail.com>
2024-09-12 12:58:12 +0530

    Updates

---
+++ precache-manifest.98fb72a64bbe79711120f3725eb76e7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40930d181e73b705bc6439da824a28de",
+    "url": "/ProjectManagementApp/index.html"
+  },
+  {
+    "revision": "c8fedce6a8ce1a5de294",
+    "url": "/ProjectManagementApp/static/css/main.70727bed.chunk.css"
+  },
+  {
+    "revision": "b1739227e42c63f9a210",
+    "url": "/ProjectManagementApp/static/js/2.34d4aac3.chunk.js"
+  },
+  {
+    "revision": "890c17097c57b70d63fcb3c2662d0bcd",
+    "url": "/ProjectManagementApp/static/js/2.34d4aac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f580af6752125678c8d",
+    "url": "/ProjectManagementApp/static/js/3.ccce38eb.chunk.js"
+  },
+  {
+    "revision": "c8fedce6a8ce1a5de294",
+    "url": "/ProjectManagementApp/static/js/main.0a5fdacd.chunk.js"
+  },
+  {
+    "revision": "7ec0991e12a0b4302cf2",
+    "url": "/ProjectManagementApp/static/js/runtime-main.9c689b4b.js"
+  }
+]);
